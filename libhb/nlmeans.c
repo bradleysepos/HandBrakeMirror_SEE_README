44e2281893a9c74461999761665f2557e230b684
Bradley Sepos <bradley@bradleysepos.com>
2014-05-27 20:09:28 -0400

    Update license comment.

--- libhb/nlmeans.c
+++ libhb/nlmeans.c
@@ -1,21 +1,12 @@
-/*
- Copyright (c) 2013 Dirk Farin
- Copyright (c) 2014 HandBrake Team
+/* nlmeans.c
 
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License along
- with this program; if not, write to the Free Software Foundation, Inc.,
- 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-*/
+   Copyright (c) 2013 Dirk Farin
+   Copyright (c) 2003-2014 HandBrake Team
+   This file is part of the HandBrake source code
+   Homepage: <http://handbrake.fr/>.
+   It may be used under the terms of the GNU General Public License v2.
+   For full terms see the file COPYING file or visit http://www.gnu.org/licenses/gpl-2.0.html
+ */
 
 #include "hb.h"
 #include "hbffmpeg.h"
